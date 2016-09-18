927b08d1a87627e9a0594a7f17023a89f773eb9d
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2016-09-17 22:14:28 -0300

    Corrigindo compilacao

--- src/components/ReactDisqus.js
+++ src/components/ReactDisqus.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 class ReactDisqus extends React.Component{
   componentDidMount() {
-    this.embedDisqus()
+    this.embedDisqus();
   }
   render() {
     return(
@@ -20,14 +20,14 @@ class ReactDisqus extends React.Component{
   }
   embedDisqus() {
     let disqus_config = function() {
-      this.page.url = this.props.pageurl
-      this.page.identifier = this.props.identifier
-    }
-    let protocol = this.props.protocol
-    let dsq = document.createElement('script')
-    dsq.type = 'text/javascript'
-    dsq.async = true
-    dsq.src = protocol + '://' + this.props.shortname + '.disqus.com/embed.js'
+      this.page.url = this.props.pageurl;
+      this.page.identifier = this.props.identifier;
+    };
+    let protocol = this.props.protocol;
+    let dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = protocol + '://' + this.props.shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq)
   }
 }
@@ -37,13 +37,13 @@ ReactDisqus.propTypes = {
   pageurl: React.PropTypes.string,
   identifier: React.PropTypes.string,
   protocol: React.PropTypes.string
-}
+};
 
 ReactDisqus.defaultProps = {
   shortname: null,
   pageurl: window.location.href,
   identifier: null,
   protocol: 'http'
-}
+};
 
 export default ReactDisqus
