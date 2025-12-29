fe61021c89e8aeadfa1e88a9422602f23edf7da3
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2025-12-29 15:45:11 -0300

    chore: improve hover transition of the card

--- src/components/shared/organisms/Card.tsx
+++ src/components/shared/organisms/Card.tsx
@@ -33,19 +33,23 @@ export function Card({ item, index, isVisible, variant }: CardProps) {
 
   return (
     <div
-      className={`transition-all duration-700 group relative border border-stone-200 dark:border-stone-800 bg-white dark:bg-stone-900 p-8 hover:border-lime-500/50 dark:hover:border-lime-400/50 ${
+      className={`transition-all duration-700 group relative bg-white dark:bg-stone-900 ${
         isVisible ? 'opacity-100 translate-y-0' : `opacity-0 ${translateY}`
       }`}
       style={transitionStyle}
     >
-      <CardHeader item={item} variant={variant} />
+      <div
+        className="p-8 min-h-full transition-all duration-700 border border-stone-200 dark:border-stone-800 hover:border-lime-500/50 dark:hover:border-lime-400/50"
+      >
+        <CardHeader item={item} variant={variant} />
 
-      <CardContent item={item} variant={variant} />
+        <CardContent item={item} variant={variant} />
 
-      <CardFooter item={item} variant={variant} />
+        <CardFooter item={item} variant={variant} />
 
-      {/* Subtle interactive accent */}
-      <div className="absolute bottom-0 right-0 w-1 h-1 bg-lime-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+        {/* Subtle interactive accent */}
+        <div className="absolute bottom-0 right-0 w-1 h-1 bg-lime-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+      </div>
     </div>
   )
 }
